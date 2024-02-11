e38e7a423e78782e096d12ec0ee4582aa6f677b3
ZinixZay <romadesl21@gmail.com>
2024-02-11 13:43:14 +0300

    admin panel set up

--- tycoon/exercises/admin.py
+++ tycoon/exercises/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Exercise, Question, Answer
 
-# Register your models here.
+
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'identifier', 'creator')
+    list_display_links = ('id', 'title', 'identifier')
+    search_fields = ('title', 'identifier')
+    list_filter = ('is_feedback', )
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'exercise', 'label', 'question_type')
+    list_display_links = ('id', )
+    search_fields = ('exercise', 'term')
+    list_filter = ('question_type', )
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'question')
+    list_display_links = ('id', )
+    search_fields = ('user', 'question')
+
+
+admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
