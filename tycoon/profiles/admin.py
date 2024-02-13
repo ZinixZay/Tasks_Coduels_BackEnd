69691f71feb493a10bf679897d54c1cd068f4357
ZinixZay <romadesl21@gmail.com>
2024-02-13 12:13:06 +0300

    admin panel updated

--- tycoon/profiles/admin.py
+++ tycoon/profiles/admin.py
@@ -3,7 +3,7 @@ from .models import TasksProfile
 
 
 class TaskProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'avatar', 'backup_email', 'user')
+    list_display = ('id', 'first_name', 'last_name', 'status', 'avatar', 'backup_email', 'user')
     list_display_links = ('id', 'user')
     search_field = ('user', 'last_name', 'first_name')
 
